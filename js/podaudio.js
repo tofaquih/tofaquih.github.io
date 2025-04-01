16d623bf57a65fbbf3f62c1b7ff8a03ab0db50ed
tofaquih <tariqf549@gmail.com>
2025-04-01 00:57:42 -0400

    added cag and vte sleep projects and images. added audio files for AI podcasts for SHS Mage and PFAS projects, added text summary json files in ar and en, updated css styles, updates index, created a new html page for audio play, moves all pdfs to a new pdfs folder

---
+++ js/podaudio.js
@@ -0,0 +1,122 @@
+
+function getQueryParam(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const headerElement = document.getElementById('pageHeader');
+    const clickedText = getQueryParam('Pod');
+    const audioPlayer = document.getElementById('myAudio');
+    const summaryElement = document.getElementById('summary');
+    const toggle = document.getElementById('LangToggle');
+
+    let englishJsonFile = './audio/summary-en.json'; // Default JSON file    
+    let arabicJsonFile = './audio/summary-ar.json'; // arabic JSON file    
+    let currentJsonFile = './audio/summary-en.json';
+    let currentDirection = '';
+    let episodeTitle = '';
+    
+
+    
+
+    if (clickedText) {
+      
+      if (clickedText == 'SHSpodcast') {
+          episodeTitle = 'Genetics of Composite Sleep Health Scores in 413,904 Individuals'
+          audioPlayer.src = `Genetics of Composite Sleep Health Scores in 413,904 Individuals.wav`
+          headerElement.textContent = episodeTitle;
+          $(".banner-image-pod").backstretch('images/SHS.jpg');
+      } else if (clickedText == 'MetaboAgePod'){
+          audioPlayer.src = "audio/Metabolomic Prediction of Age Using Endogenous and Xenobiotic Metabolites.wav"
+          episodeTitle = 'Metabolomic Prediction of Age Using Endogenous and Xenobiotic Metabolites'
+          headerElement.textContent = episodeTitle;  
+          $(".banner-image-pod").backstretch('images/portfolio-7.png');
+      } else if (clickedText == 'pfaspod'){
+        audioPlayer.src = "audio/PFAS Exposure Links to Unfavorable Cardio-Metabolic Profiles.wav"
+        episodeTitle = 'PFAS Exposure Links to Unfavorable Cardio-Metabolic Profiles'
+        headerElement.textContent = episodeTitle;   
+        $(".banner-image-pod").backstretch('images/portfolio-5.png');
+
+    }
+      
+    } else {
+      headerElement.textContent = 'Welcome!'; // Optional default text
+    }
+
+
+    function setInitialState() {
+        const savedState = localStorage.getItem('lang');
+        let isArabic = false;
+        if (savedState === 'ar' ) {
+            isArabic = true;
+            toggle.checked = true;
+            currentDirection = 'rtl';
+            // summaryElement.dir = 
+            currentJsonFile = arabicJsonFile;
+        } else {
+            toggle.checked = false;
+            // summaryElement.dir = 'ltr';
+            currentDirection = 'ltr';
+
+            currentJsonFile = englishJsonFile;
+        }
+    }
+
+    setInitialState();
+
+    function loadSummary(jsonFile, pDirection) {
+        fetch(jsonFile)
+            .then(response => response.json())
+            .then(data => {
+                let summary = data[clickedText] || 'Summary not available for this Project.';
+                const htmlSummary = summary
+                    .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+                    .replace(/_(.*?)_/g, '<em>$1</em>')
+                    .replace(/^- (.*)$/gm, '<li>$1</li>')
+                    .replace(/\n/g, '<br>');
+                summaryElement.innerHTML = htmlSummary;
+                summaryElement.dir = pDirection;
+            })
+            .catch(error => {
+                summaryElement.textContent = 'Failed to load summary.';
+            });
+    }
+
+    loadSummary(currentJsonFile, currentDirection);
+
+    LangToggle.addEventListener('change', function() {
+        if (this.checked) {
+            currentJsonFile = arabicJsonFile; 
+            loadSummary(currentJsonFile, 'rtl');
+            localStorage.setItem('lang', 'ar');
+            
+            
+        } else {
+            currentJsonFile = englishJsonFile; 
+            loadSummary(currentJsonFile, 'ltr');
+            localStorage.setItem('lang', 'en');
+
+        }
+    });
+
+    
+    // fetch(englishJsonFile)
+    // .then(response => response.json())
+    // .then(data => {
+    //   summary = data[clickedText] || 'Summary not available for this Project.';
+    //   // Convert Markdown to HTML here:
+    //   const htmlSummary = summary
+    //     .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Bold
+    //     .replace(/_(.*?)_/g, '<em>$1</em>')       // Italics
+    //     .replace(/^- (.*)$/gm, '<li>$1</li>')  // List items
+    //     .replace(/\n/g, '<br>');           // Newlines
+
+    //   summaryElement.innerHTML = htmlSummary; // Use innerHTML
+    //    })
+    // .catch(error => {
+    //   summaryElement.textContent = 'Failed to load summary.';
+    // });
+
+    
+  });
